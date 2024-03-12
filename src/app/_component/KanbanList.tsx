cd4fd51a2dc48ce76e87a1658698d1f94efd0124
oluzr <olu-zr@naver.com>
2024-03-12 21:38:39 +0900

    :construction: Add : dnd 코드 초기 셋팅
    기본 코드 셋팅

--- src/app/_component/KanbanList.tsx
+++ src/app/_component/KanbanList.tsx
@@ -1,9 +1,72 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Card from "./Card";
 import styled from "styled-components";
-import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
+
+export const KanbanListWrap = () => {
+  const [enabled, setEnabled] = useState(false);
+  const items = [...Array(4)].map((_, i) => ({
+    id: `${i}${i}${i}`,
+    content: `item-${i}`,
+  }));
+
+  // --- Draggable이 Droppable로 드래그 되었을 때 실행되는 이벤트
+  const onDragEnd = ({ source, destination }: DropResult) => {
+    console.log(">>> source", source);
+    console.log(">>> destination", destination);
+  };
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+  if (!enabled) {
+    return null;
+  }
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="droppable">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {item.content}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+const KanbanList = ({ title }: { title: string }) => {
+  return (
+    <List>
+      <h3 className="text-2xl">{title}</h3>
+      <ul></ul>
+    </List>
+  );
+};
 const ListWrap = styled.div`
   display: flex;
   gap: 10px;
@@ -23,27 +86,3 @@ const List = styled.div`
     display: flex;
   }
 `;
-
-const titleList = ["진행전", "진행중", "완료"];
-export const KanbanListWrap = () => {
-  return (
-    <>
-      <ListWrap>
-        {titleList.map((item, idx) => (
-          <KanbanList key={idx} title={item} />
-        ))}
-      </ListWrap>
-    </>
-  );
-};
-
-const KanbanList = ({ title }: { title: string }) => {
-  return (
-    <List>
-      <h3 className="text-2xl">{title}</h3>
-      <ul>
-        <Card />
-      </ul>
-    </List>
-  );
-};
